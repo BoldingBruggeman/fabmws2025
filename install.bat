730143d9362ac984f49d55f554a9df7675d55c1f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-06-06 10:38:41 +0100

    windows: use call for other bat file

--- install.bat
+++ install.bat
@@ -3,6 +3,6 @@ pip install -v code\fabm
 cmake -B build/gotm -S code/gotm -DFABM_BASE=code/fabm -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX%
 cmake --build build/gotm --target install
 
-code\pygetm\install.bat
+call code\pygetm\install.bat
 
 pip install -v code\fabmos
