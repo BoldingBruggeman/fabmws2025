83ba650a38e3394c516a92098f7f5864e3f05746
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-06-09 09:08:11 +0100

    test simulations

---
+++ .github/workflows/run_global.py
@@ -0,0 +1,56 @@
+import cftime
+import os
+import datetime
+
+import fabmos
+import fabmos.transport.tmm
+import fabmos.input.riverlist
+
+import pandas as pd
+
+script_dir = os.path.dirname(__file__)
+tm_config_dir = os.path.join(script_dir, "../../setups/global-fabmos"
+calendar = "360_day"  # any valid calendar recognized by cftime, see https://cfconventions.org/cf-conventions/cf-conventions.html#calendar
+
+fabm_yaml = os.path.join(
+    script_dir, "../../extern/fabm/extern/mops/testcases/fabm_with_runoff.yaml"
+)
+
+domain = fabmos.transport.tmm.create_domain(tm_config_dir)
+
+sim = fabmos.transport.tmm.Simulator(domain, calendar=calendar, fabm=fabm_yaml)
+
+sim.fabm.get_dependency("mole_fraction_of_carbon_dioxide_in_air").set(280.0)
+sim.fabm.get_dependency("surface_air_pressure").set(101325.0)
+
+# Load rivers from https://doi.org/10.1029/96JD00932
+rivers = pd.read_fwf(
+    os.path.join(script_dir, "rivrstat.txt"),
+    skiprows=7,
+    usecols=(1, 2, 3, 4, 6),
+    names=("name", "country", "lat", "lon", "flow"),
+)
+
+# Exclude Arctic rivers as in https://doi.org/10.5194/bg-10-8401-2013
+rivers = rivers.loc[rivers.lat <= 60.0]
+
+# Convert river list to gridded [2D] river inputs (level increase in m s-1)
+runoff = fabmos.input.riverlist.map_to_grid(sim.T, rivers.lon, rivers.lat, rivers.flow)
+
+# Reintroduce buried phosphorus, weighted by river runoff
+# (https://doi.org/10.5194/bg-10-8401-2013)
+sim.request_redistribution(
+    "det/burial", "runoff/source", runoff, datetime.timedelta(days=360)
+)
+
+out = sim.output_manager.add_netcdf_file(
+    "output.nc", interval=1, interval_units=fabmos.TimeUnit.MONTHS, save_initial=False
+)
+out.request("runoff_source", *sim.fabm.default_outputs, time_average=True)
+
+start = cftime.datetime(2000, 1, 1, calendar=calendar)
+stop = cftime.datetime(2001, 1, 1, calendar=calendar)
+sim.start(start, timestep=12 * 3600)
+while sim.time < stop:
+    sim.advance()
+sim.finish()
---
+++ .github/workflows/run_north_sea.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os.path
+
+import pygetm
+import pygetm.legacy
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "setup_dir",
+    help="Path to configuration files (NorthSea directory from https://sourceforge.net/p/getm/getm-setups)",
+    default=".",
+    nargs="?",
+)
+args = parser.parse_args()
+
+domain = pygetm.legacy.domain_from_topo(
+    os.path.join(args.setup_dir, "Topo/NS6nm.v01.nc"), z0=0.001
+)
+pygetm.legacy.load_bdyinfo(domain, os.path.join(args.setup_dir, "bdyinfo.dat"))
+pygetm.legacy.load_riverinfo(domain, os.path.join(args.setup_dir, "riverinfo.dat"))
+
+sim = pygetm.Simulation(
+    domain,
+    gotm=os.path.join(args.setup_dir, "gotmturb.nml"),
+    airsea=pygetm.airsea.FluxesFromMeteo(
+        humidity_measure=pygetm.HumidityMeasure.SPECIFIC_HUMIDITY,
+        calculate_evaporation=True,
+    ),
+    internal_pressure=pygetm.internal_pressure.ShchepetkinMcwilliams(),
+    vertical_coordinates=pygetm.vertical_coordinates.GVC(
+        30, Dgamma=40.0, ddu=0.75, ddl=0.5
+    ),
+    Dcrit=0.2,
+    Dmin=0.05,
+)
+
+sim.logger.info("Reading 2D boundary data from file")
+bdy_2d_path = os.path.join(args.setup_dir, "Forcing/2D/bdy.2d.2006.nc")
+domain.open_boundaries.z.set(pygetm.input.from_nc(bdy_2d_path, "elev"))
+domain.open_boundaries.u.set(pygetm.input.from_nc(bdy_2d_path, "u"))
+domain.open_boundaries.v.set(pygetm.input.from_nc(bdy_2d_path, "v"))
+
+bdy_3d_path = os.path.join(args.setup_dir, "Forcing/3D/bound_3D.CFSR.2006.nc")
+domain.open_boundaries.sponge.tmrlx = True
+sim.temp.open_boundaries.type = pygetm.SPONGE
+sim.temp.open_boundaries.values.set(pygetm.input.from_nc(bdy_3d_path, "temp"))
+sim.salt.open_boundaries.type = pygetm.SPONGE
+sim.salt.open_boundaries.values.set(pygetm.input.from_nc(bdy_3d_path, "salt"))
+
+river_path = os.path.join(args.setup_dir, "Forcing/River/rivers.nc")
+for name, river in domain.rivers.items():
+    river.flow.set(pygetm.input.from_nc(river_path, name))
+    river["salt"].set(0.5)
+
+sim.radiation.set_jerlov_type(pygetm.Jerlov.Type_II)
+sim.temp.set(11.6)
+sim.salt.set(35.2)
+sim.density.convert_ts(sim.salt, sim.temp)
+
+sim.logger.info("Setting up meteorological forcing")
+met_path = os.path.join(args.setup_dir, "Forcing/Meteo/CFSR.daymean.2006.nc")
+sim.airsea.tcc.set(pygetm.input.from_nc(met_path, "tcc"))
+sim.airsea.t2m.set(pygetm.input.from_nc(met_path, "t2"))
+sim.airsea.qa.set(pygetm.input.from_nc(met_path, "sh"))
+sim.airsea.sp.set(pygetm.input.from_nc(met_path, "slp"))
+sim.airsea.u10.set(pygetm.input.from_nc(met_path, "u10"))
+sim.airsea.v10.set(pygetm.input.from_nc(met_path, "v10"))
+sim.airsea.tp.set(pygetm.input.from_nc(met_path, "precip"))
+
+sim.logger.info("Setting up output")
+output = sim.output_manager.add_netcdf_file(
+    "north_sea_3d.nc", interval=datetime.timedelta(days=1)
+)
+output.request("temp", "salt", "tke", "num", "SS", "NN")
+
+sim.start(datetime.datetime(2006, 1, 2), timestep=60.0, split_factor=30, report=60)
+while sim.time < datetime.datetime(2006, 1, 10):
+    sim.advance()
+sim.finish()
